1c2706a024501615d3a8ac5789d1235289d6f10f
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-04-27 22:39:20 +0300

    Hotels Create-Update Action/Views ok

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -11,9 +11,38 @@ class HotelsController < ApplicationController
     @hotel = Hotel.new
   end
 
+  def create
+    @hotel = Hotel.new(hotel_params)
+
+    if @hotel.save
+      redirect_to(hotels_path)
+    else
+      render('new')
+    end
+  end
+
   def edit
+    @hotel = Hotel.find(params[:id])
+  end
+
+  def update
+    @hotel = Hotel.find(params[:id])
+    
+    if @hotel.update_attributes(hotel_params)
+      redirect_to(hotel_path(@hotel.id))
+    else
+      render('edit')
+    end
   end
 
   def delete
+   # @hotel = Hotel.find(params[:id])
   end
+
+  private
+    def hotel_params
+      params.require(:hotel).permit(:name, :city, :street, :street_number, :zip_code, :rating, :construction_year, :renovation_year)
+    end
+
+
 end
