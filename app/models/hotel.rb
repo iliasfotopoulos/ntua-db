1ec8f798a900a621e3155c2e5c900977c3f143b6
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-04-29 19:32:18 +0300

    Added data valaditon on Hotel Model

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -3,4 +3,20 @@ class Hotel < ActiveRecord::Base
 	has_one :hotel_phone
 	has_many :rooms
 
+
+	validates :name, :city, presence: true
+	validates :street_number, numericality: true
+	validates :rating, numericality: true, 
+	inclusion: {in: 0..5, message: "rating should be 0-5"}
+
+	validates :construction_year, :renovation_year,
+	inclusion: { in: 1800..Date.today.year, message: "Year should be over 1800"},
+  	allow_nil: true,
+  	format: 
+  	{ 
+    	with: /(18|19|20)\d{2}/i, 
+    	message: "should be a four-digit year"
+  	}
+
+
 end
