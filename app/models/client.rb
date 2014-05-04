f34c0c234a9cac279e4bf00c4fee3a12b0be2a55
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-05-04 22:04:15 +0300

    Validation for Clients Model

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,9 @@ class Client < ActiveRecord::Base
 	has_one :client_phone
 	has_many :reservations
 
+	validates :identity, :first_name, :last_name, presence: true
+	validates :identity, uniqueness: {message: "This identity already exists. Identity must be unique!"}
+	validates :street_number, numericality: true
+
+
 end
