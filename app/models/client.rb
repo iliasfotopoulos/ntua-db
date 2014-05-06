5dfb19a382dfeeb2661c9e9ec73335844ecf0f9b
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-05-06 22:41:21 +0300

    Remove dependent: destroy in client model (created trigger)

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,7 @@
 class Client < ActiveRecord::Base
 
 	has_one :client_phone
-	has_many :reservations, dependent: :destroy
+	has_many :reservations #, dependent: :destroy
 
 	validates :identity, :first_name, :last_name, presence: true
 	validates :identity, uniqueness: {message: "This identity already exists. Identity must be unique!"}
