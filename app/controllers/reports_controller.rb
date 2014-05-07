663d43b3e7ea517ca72a45b2c470805f5c51b989
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-05-07 19:11:36 +0300

    Reservations per room type -> Raw SQL

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,20 @@
 class ReportsController < ApplicationController
   def report
-  	@room_reservations = Reservation.group(:room_id).count
-  	@room_type_reservations = Reservation.joins(:room).group(:room_type).count
+  	#@room_type_reservations = Reservation.joins(:room).group(:room_type).count
 
   	@hotel_reservations = Reservation.find_by_sql("SELECT COUNT(*) as count, hotels.name as name
 	FROM reservations
 	INNER JOIN hotels
 	ON reservations.hotel_id = hotels.id
-	GROUP BY hotels.name")
+	GROUP BY hotels.id")
+
+	@room_type_reservations = Reservation.find_by_sql("SELECT COUNT(*) as count, rooms.room_type as room_type
+	FROM reservations
+	INNER JOIN rooms
+	ON reservations.room_id = rooms.id
+	GROUP BY rooms.room_type")
+
+
 
   end
 end
