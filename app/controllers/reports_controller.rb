f5d5f19f76c75477bdea0c1bcf7d4faba5a1037f
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-05-07 19:04:02 +0300

    Reservations per hotel in raw SQL

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,13 @@
 class ReportsController < ApplicationController
   def report
-  	@hotel_reservations = Reservation.joins(:hotel).group(:name).count
   	@room_reservations = Reservation.group(:room_id).count
   	@room_type_reservations = Reservation.joins(:room).group(:room_type).count
+
+  	@hotel_reservations = Reservation.find_by_sql("SELECT COUNT(*) as count, hotels.name as name
+	FROM reservations
+	INNER JOIN hotels
+	ON reservations.hotel_id = hotels.id
+	GROUP BY hotels.name")
 
   end
 end
