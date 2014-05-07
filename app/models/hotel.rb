516214fe71351900063b016e7e41b7b47982aae5
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-05-07 14:14:18 +0300

    Hotel & Reservation model -> has_many: through:

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,6 +2,7 @@ class Hotel < ActiveRecord::Base
 
 	has_one :hotel_phone
 	has_many :rooms #, dependent: :destroy
+	has_many :reservations, through: :rooms
 
 
 	validates :name, :city, presence: true
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,6 @@ class Reservation < ActiveRecord::Base
 
 	belongs_to :client
 	belongs_to :room
+	has_one :hotel, through: :room
 
 end
