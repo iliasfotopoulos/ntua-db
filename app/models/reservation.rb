33d6f2dc1a7a9c22e53a36e4c82e79dfd4d906f5
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-05-07 20:12:25 +0300

    Add validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,28 @@
 class Reservation < ActiveRecord::Base
 
 	belongs_to :client
+	validates :client_id, presence: true
+
 	belongs_to :room
+	validates :room_id, presence: true
+
 	has_one :hotel, through: :room
+	validates :hotel_id, presence: true
+
+	validates :arrival_date, :departure_date, presence: true
+
+	validate :arrival_before_departure, :arrival_date_cannot_be_in_the_past, :departure_date_cannot_be_in_the_past
+
+	def arrival_before_departure
+	    errors.add(:arrival_date, "must be before departure date") if arrival_date >= departure_date
+  	end
+
+  	def arrival_date_cannot_be_in_the_past
+	    errors.add(:arrival_date, "can't be in the past") if arrival_date < Date.today
+  	end
+
+  	def departure_date_cannot_be_in_the_past
+	    errors.add(:departure_date, "can't be in the past") if departure_date < Date.today
+  	end  
 
 end
