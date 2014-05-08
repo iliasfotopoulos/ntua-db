cfb010a4c7e84b9355eac64dfeb67da3b9b41ae8
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-05-08 19:28:41 +0300

    Triggers format to postgresql

--- db/migrate/20140507095633_create_trigger_delete_hotel.rb
+++ db/migrate/20140507095633_create_trigger_delete_hotel.rb
@@ -2,11 +2,14 @@ class CreateTriggerDeleteHotel < ActiveRecord::Migration
   def up
   	create_trigger = "CREATE TRIGGER delete_hotel_rooms
   			BEFORE DELETE ON hotels 
-  			FOR EACH ROW
-  			BEGIN
-  				DELETE FROM rooms WHERE OLD.id = hotel_id;
-  			END"
+  			FOR EACH ROW EXECUTE PROCEDURE delete_rooms()"
 
+        delete_rooms_function ="CREATE FUNCTION delete_rooms() RETURNS TRIGGER AS $_$
+        BEGIN
+            DELETE FROM rooms WHERE OLD.id = hotel_id;
+        END $_$ LANGUAGE 'plpgsql';"
+
+    execute delete_rooms_function    
   	execute create_trigger
   end
 
--- db/migrate/20140507095733_create_trigger_delete_reservations.rb
+++ db/migrate/20140507095733_create_trigger_delete_reservations.rb
@@ -1,12 +1,15 @@
 class CreateTriggerDeleteReservations < ActiveRecord::Migration
   def up
   	create_trigger = "CREATE TRIGGER delete_client_reservations
-  			BEFORE DELETE ON clients 
-  			FOR EACH ROW
-  			BEGIN
-  				DELETE FROM reservations WHERE OLD.id = client_id;
-  			END"
+  			BEFORE DELETE ON clients
+  			FOR EACH ROW EXECUTE PROCEDURE delete_reservations()"
 
+    delete_reservations_function ="CREATE FUNCTION delete_reservations() RETURNS TRIGGER AS $_$
+        BEGIN
+            DELETE FROM reservations WHERE OLD.id = client_id;
+        END $_$ LANGUAGE 'plpgsql"
+
+    execute delete_reservations_function   
   	execute create_trigger
   end
 
