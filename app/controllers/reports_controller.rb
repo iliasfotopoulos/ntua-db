650a0aa534e2854ce6cade0bc18ac3b6128c1bc8
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-05-08 21:57:51 +0300

    Queries to Postgresql

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,19 +2,19 @@ class ReportsController < ApplicationController
   def report
   	#@room_type_reservations = Reservation.joins(:room).group(:room_type).count
 
-  	@hotel_reservations = Reservation.find_by_sql("SELECT COUNT(*) as count, hotels.name as name
+  	@hotel_reservations = Reservation.find_by_sql("SELECT COUNT(reservations.id) AS count, hotels.name AS name
 	FROM reservations
 	INNER JOIN hotels
 	ON reservations.hotel_id = hotels.id
 	GROUP BY hotels.id")
 
-	@room_type_reservations = Reservation.find_by_sql("SELECT COUNT(*) as count, rooms.room_type as room_type
+	@room_type_reservations = Reservation.find_by_sql("SELECT COUNT(reservations.id) AS count, rooms.room_type AS room_type
 	FROM reservations
 	INNER JOIN rooms
 	ON reservations.room_id = rooms.id
 	GROUP BY rooms.room_type")
 
-	@hotel_rooms_having = Room.find_by_sql("SELECT COUNT(*) as count, hotels.name as name
+	@hotel_rooms_having = Room.find_by_sql("SELECT COUNT(rooms.id) AS count, hotels.name AS name
 	FROM rooms
 	INNER JOIN hotels
 	ON rooms.hotel_id = hotels.id
